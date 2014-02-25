79a9232e90b5890c01217f6a3db290d445cc7662
TimCamp2 <timcamp2@gmail.com>
2014-02-25 12:44:39 -0500

    Added jQuery button color change

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.button').mouseover(function() {
+    $(this).addClass('hover');
+});
+  $('.button').mouseout(function() {
+    $(this).removeClass('hover');
+});
+});
